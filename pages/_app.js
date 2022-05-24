54450c0e6cae4d148eedefd698db4fc35673db31
lechiffre4 <toxcoa@gmail.com>
2022-05-24 12:56:55 -0400

    react 18

--- pages/_app.js
+++ pages/_app.js
@@ -38,7 +38,7 @@ function MyApp({ Component, pageProps }) {
       <Script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"/>
       <Script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"/>
       <Script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"/>
-      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>      
       
     </ThirdwebWeb3Provider>
 
