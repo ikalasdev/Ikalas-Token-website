c85cb09b0b24493ed3cf6a1e8fd070eac7f33036
lechiffre4 <toxcoa@gmail.com>
2022-06-03 09:40:08 -0400

    correct compilation errors + add link to news caroussel

--- component/News.js
+++ component/News.js
@@ -26,22 +26,27 @@ const News = () => {
                     <div className="keen-slider__slide number-slide1 d-flex flex-column justify-content-center">
                         <h3 className="display-5 fw-bold text-dark">20/05/2022</h3>
                         <span className="text-muted lead" style={{fontSize:"25px"}}>{_tr("NewsText1")}</span>
+                        <a className="text-info lead" href="https://www.kik-token.com/fr" target="_blank" rel="noreferrer" style={{fontSize:"25px"}}>Go on the website</a>
                     </div>
                     <div className="keen-slider__slide number-slide2 d-flex flex-column justify-content-center">
                         <h3 className="display-5 fw-bold text-dark">05/2022</h3>
                         <span className="text-muted lead" style={{fontSize:"25px"}}>{_tr("NewsText2")}</span>
+                        <a className="text-info lead" href="https://ikalas.com/app/create-token-on-bsc" target="_blank"  style={{fontSize:"25px"}}>Go to this module</a>
                     </div>
                     <div className="keen-slider__slide number-slide3  d-flex flex-column justify-content-center">
                         <h3 className="display-5 fw-bold text-dark">12/2021</h3>
                         <span className="text-muted lead" style={{fontSize:"25px"}}>{_tr("NewsText3")}</span>
+                        <a className="text-info lead" href="https://ikalas.com/app/matic-token-multisender" target="_blank" rel="noreferrer" style={{fontSize:"25px"}}>Go to this module</a>
                     </div>
                     <div className="keen-slider__slide number-slide4  d-flex flex-column justify-content-center">
                         <h3 className="display-5 fw-bold text-dark">07/2021</h3>
                         <span className="text-muted lead" style={{fontSize:"25px"}}>{_tr("NewsText4")}</span>
+                        <a className="text-info lead" href="https://ikalas.com/app/bsc-token-multisender" target="_blank" rel="noreferrer" style={{fontSize:"25px"}}>Go to this module</a>
                     </div>
                     <div className="keen-slider__slide number-slide5  d-flex flex-column justify-content-center">
                         <h3 className="display-5 fw-bold text-dark">01/2021</h3>
                         <span className="text-muted lead" style={{fontSize:"25px"}}>{_tr("NewsText5")}</span>
+                        <a className="text-info lead" href="https://ikalas.com/app" target="_blank" rel="noreferrer" style={{fontSize:"25px"}}>Go on Ikalas</a>
                     </div>
                 </div>
                 {loaded && instanceRef.current && (
